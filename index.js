d07ea0175f6aa753f1195f922cd4b5f2f3e11a19
Gianmarco <GSCordova7@gmail.com>
2018-03-05 02:02:01 +0100

    Creando clases, creando primeraPag y varios

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ class Pokemon{
         this.vida = vida;
         this.tipo = tipo
     }
+
     ataqueBasico(){return basico()}
 
     ataqueEspecial(){return especial()}
@@ -25,8 +26,27 @@ class Pokemon{
 
 let pikachu = new Pokemon('Pikachu', 100, 'electrico')
 
+//Creacion de variable
+
+const primerElegido = document.getElementById('seleccionado1')
+const segundoElegido = document.getElementById('seleccionado2')
+const innerElegir = '{Sin Elegir}'
+
 //Funciones Globales
 
 let basico = () => random(0, 10)
 let especial = () => random(5, 20)
 let random = (min, max) => Math.floor( Math.random() * (max - min  + 1) ) + min
+let resetearPrimero = () => primerElegido.innerHTML = innerElegir
+let resetearSegundo = () => segundoElegido.innerHTML = innerElegir
+let seleccion = (event) => {
+    console.log(event.target.innerHTML)
+    if(primerElegido.innerHTML == innerElegir){
+        primerElegido.innerHTML = event.target.innerHTML
+    } else if (segundoElegido.innerHTML == innerElegir){
+        segundoElegido.innerHTML = event.target.innerHTML
+    } else {
+        console.log('Ya has elegido a los 2 pokemon. No puedes cambiar')
+    }
+
+}
